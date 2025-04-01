89be90d362dbf6a88706ab36111bc8db6dc88702
Sylvain Girard <zesly42@gmail.com>
2025-04-01 23:03:06 +0200

    Send sensors values only when they change. Set precision to 0.1

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,16 @@
 // humidity and pressure measurment to a zigbee network.
 //
 // This software is governed by the CeCILL-B license under French law and
-// abiding by the rules of distribution of free software.  You can  use, 
+// abiding by the rules of distribution of free software.  You can  use,
 // modify and/ or redistribute the software under the terms of the CeCILL-B
 // license as circulated by CEA, CNRS and INRIA at the following URL
-// "http://www.cecill.info". 
+// "http://www.cecill.info".
 //
 // As a counterpart to the access to the source code and  rights to copy,
 // modify and redistribute granted by the license, users are provided only
 // with a limited warranty  and the software's author,  the holder of the
 // economic rights,  and the successive licensors  have only  limited
-// liability. 
+// liability.
 //
 // In this respect, the user's attention is drawn to the risks associated
 // with loading,  using,  modifying and/or developing or reproducing the
@@ -23,9 +23,9 @@
 // therefore means  that it is reserved for developers  and  experienced
 // professionals having in-depth computer knowledge. Users are therefore
 // encouraged to load and test the software's suitability as regards their
-// requirements in conditions enabling the security of their systems and/or 
-// data to be ensured and,  more generally, to use and operate it in the 
-// same conditions as regards security. 
+// requirements in conditions enabling the security of their systems and/or
+// data to be ensured and,  more generally, to use and operate it in the
+// same conditions as regards security.
 //
 // The fact that you are presently reading this means that you have had
 // knowledge of the CeCILL-B license and that you accept its terms.
@@ -44,7 +44,7 @@
 #define PRESSURE_SENSOR_ENDPOINT_NUMBER 11
 
 #define uS_TO_S_FACTOR 1000000ULL /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  55         /* Sleep for 55s will + 5s delay for establishing connection => data reported every 1 minute */
+#define TIME_TO_SLEEP 58          /* Sleep for 58 will + 2s delay for establishing connection => data reported every 1 minute */
 
 uint8_t button = BOOT_PIN;
 
@@ -55,28 +55,57 @@ Adafruit_BME280 bme;
 
 uint8_t getBatteryCapacity();
 
-void readSensors()
+struct SensorsValues
 {
-  float temperature_value = bme.readTemperature();
-  float humidity_value = bme.readHumidity();
-  float pressure_value = bme.readPressure() / 100.0;
-  uint8_t battery = getBatteryCapacity();
+  float temperature;
+  float humidity;
+  float pressure;
+  uint8_t battery;
+};
 
-  Serial.printf("Temperature %.2f °C\r\n", temperature_value);
-  zbTempSensor.setTemperature(temperature_value);
+RTC_DATA_ATTR SensorsValues LatestSensorsValues{-500.0, -1.0, 0.0, 0};
 
-  Serial.printf("Humidity    %.2f %%\r\n", humidity_value);
-  zbTempSensor.setHumidity(humidity_value);
+void SendSensorsValues(SensorsValues &sensorsValues)
+{
+  bool report = false;
 
-  Serial.printf("Pressure    %.1f hPa\r\n", pressure_value);
-  zbPressureSensor.setPressure(pressure_value);
+  if (LatestSensorsValues.temperature != sensorsValues.temperature ||
+      LatestSensorsValues.humidity != sensorsValues.humidity)
+  {
+    Serial.printf("Temperature %.2f °C\r\n", sensorsValues.temperature);
+    zbTempSensor.setTemperature(sensorsValues.temperature);
+    LatestSensorsValues.temperature = sensorsValues.temperature;
 
-  Serial.printf("Battery     %u %%\r\n", battery);
-  zbTempSensor.setBatteryPercentage(battery);
+    Serial.printf("Humidity    %.2f %%\r\n", sensorsValues.humidity);
+    zbTempSensor.setHumidity(sensorsValues.humidity);
+    LatestSensorsValues.humidity = sensorsValues.humidity;
 
-  zbTempSensor.reportBatteryPercentage();
-  zbTempSensor.report();
-  zbPressureSensor.report();
+    zbTempSensor.report();
+    report = true;
+  }
+
+  if (LatestSensorsValues.pressure != sensorsValues.pressure)
+  {
+    Serial.printf("Pressure    %.1f hPa\r\n", sensorsValues.pressure);
+    zbPressureSensor.setPressure(sensorsValues.pressure);
+    zbPressureSensor.report();
+    LatestSensorsValues.pressure = sensorsValues.pressure;
+    report = true;
+  }
+
+  if (LatestSensorsValues.battery != sensorsValues.battery)
+  {
+    Serial.printf("Battery     %u %%\r\n", sensorsValues.battery);
+    zbTempSensor.setBatteryPercentage(sensorsValues.battery);
+    zbTempSensor.reportBatteryPercentage();
+    LatestSensorsValues.battery = sensorsValues.battery;
+    report = true;
+  }
+
+  if (!report)
+  {
+    Serial.print("Nothing to report");
+  }
   Serial.println();
 }
 
@@ -114,88 +143,105 @@ void setup()
   // Init battery ADC
   pinMode(A0, INPUT);
 
-  // Activate RF switch control
-  pinMode(WIFI_ENABLE, OUTPUT); 
-  digitalWrite(WIFI_ENABLE, LOW);
-
-  delay(100);
-
-  // Use external antenna
-  pinMode(WIFI_ANT_CONFIG, OUTPUT);
-  digitalWrite(WIFI_ANT_CONFIG, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
 
   if (!bme.begin(0x76))
   {
     Serial.println("BME280 not found !");
   }
 
-  digitalWrite(LED_BUILTIN, HIGH);
-
   // Configure the wake up source and set to wake up every 5 seconds
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
-  // Optional: set Zigbee device name and model
-  zbTempSensor.setManufacturerAndModel("ZeSly", "WeatherSensor");
+  SensorsValues sensorsValues;
+  sensorsValues.temperature = std::roundf(bme.readTemperature() * 10.0) / 10.0;
+  sensorsValues.humidity = std::roundf(bme.readHumidity() * 10.0) / 10.0;
+  sensorsValues.pressure = std::roundf(bme.readPressure() / 10.0) / 10.0;
+  sensorsValues.battery = getBatteryCapacity();
+  Serial.printf("Sensors: %.2f°C %.2f%% %.2fhPa batt:%u%%\r\n",
+                sensorsValues.temperature,
+                sensorsValues.humidity,
+                sensorsValues.pressure,
+                sensorsValues.battery);
 
-  // Set minimum and maximum temperature measurement value (10-50°C is default range for chip temperature measurement)
-  zbTempSensor.setMinMaxValue(-40, 60);
-
-  // Set tolerance for temperature measurement in °C (lowest possible value is 0.01°C)
-  zbTempSensor.setTolerance(1);
-
-  // Set power source to battery and set battery percentage to measured value
-  // The value can be also updated by calling zbTempSensor.setBatteryPercentage(percentage) anytime
-  uint8_t battery = getBatteryCapacity();
-  zbTempSensor.setPowerSource(ZB_POWER_SOURCE_BATTERY, battery);
-
-  // Set minimum and maximum pressure measurement value in hPa
-  zbPressureSensor.setMinMaxValue(0, 10000);
-
-  // Optional: Set tolerance for pressure measurement in hPa
-  zbPressureSensor.setTolerance(1);
-
-  // Add humidity cluster to the temperature sensor device with min, max and tolerance values
-  zbTempSensor.addHumiditySensor(0, 100, 1);
-
-  // Add endpoints to Zigbee Core
-  Zigbee.addEndpoint(&zbTempSensor);
-  Zigbee.addEndpoint(&zbPressureSensor);
-
-  Serial.println("Starting Zigbee...");
-  // Create a custom Zigbee configuration for End Device with keep alive 10s to avoid interference with reporting data
-  esp_zb_cfg_t zigbeeConfig = ZIGBEE_DEFAULT_ED_CONFIG();
-  zigbeeConfig.nwk_cfg.zed_cfg.keep_alive = 10000;
-
-  // When all EPs are registered, start Zigbee in End Device mode
-  if (!Zigbee.begin(&zigbeeConfig, false))
+  if (sensorsValues.temperature != LatestSensorsValues.temperature ||
+      sensorsValues.humidity != LatestSensorsValues.humidity ||
+      sensorsValues.pressure != LatestSensorsValues.pressure ||
+      sensorsValues.battery != LatestSensorsValues.battery ||
+      digitalRead(button) == LOW)
   {
-    Serial.println("Zigbee failed to start!");
-    Serial.println("Rebooting...");
-    digitalWrite(LED_BUILTIN, LOW);
-    ESP.restart();
-  }
-  else
-  {
-    Serial.println("Zigbee started successfully!");
-  }
-  Serial.println("Connecting to network");
-  while (!Zigbee.connected())
-  {
-    Serial.print(".");
+
+    // Activate RF switch control
+    pinMode(WIFI_ENABLE, OUTPUT);
+    digitalWrite(WIFI_ENABLE, LOW);
+
     delay(100);
+
+    // Use external antenna
+    pinMode(WIFI_ANT_CONFIG, OUTPUT);
+    digitalWrite(WIFI_ANT_CONFIG, HIGH);
+
+    // Optional: set Zigbee device name and model
+    zbTempSensor.setManufacturerAndModel("ZeSly", "WeatherSensor");
+
+    // Set minimum and maximum temperature measurement value (10-50°C is default range for chip temperature measurement)
+    zbTempSensor.setMinMaxValue(-40, 60);
+
+    // Set tolerance for temperature measurement in °C (lowest possible value is 0.1°C)
+    zbTempSensor.setTolerance(10);
+
+    // Set power source to battery and set battery percentage to measured value
+    zbTempSensor.setPowerSource(ZB_POWER_SOURCE_BATTERY, sensorsValues.battery);
+
+    // Set minimum and maximum pressure measurement value in hPa
+    zbPressureSensor.setMinMaxValue(0, 10000);
+
+    // Optional: Set tolerance for pressure measurement in hPa
+    zbPressureSensor.setTolerance(10);
+
+    // Add humidity cluster to the temperature sensor device with min, max and tolerance values
+    zbTempSensor.addHumiditySensor(0, 100, 10);
+
+    // Add endpoints to Zigbee Core
+    Zigbee.addEndpoint(&zbTempSensor);
+    Zigbee.addEndpoint(&zbPressureSensor);
+
+    Serial.println("Starting Zigbee...");
+    // Create a custom Zigbee configuration for End Device with keep alive 10s to avoid interference with reporting data
+    esp_zb_cfg_t zigbeeConfig = ZIGBEE_DEFAULT_ED_CONFIG();
+    zigbeeConfig.nwk_cfg.zed_cfg.keep_alive = 10000;
+
+    // When all EPs are registered, start Zigbee in End Device mode
+    if (!Zigbee.begin(&zigbeeConfig, false))
+    {
+      Serial.println("Zigbee failed to start!");
+      Serial.println("Rebooting...");
+      digitalWrite(LED_BUILTIN, LOW);
+      ESP.restart();
+    }
+    else
+    {
+      Serial.println("Zigbee started successfully!");
+    }
+    Serial.println("Connecting to network");
+    while (!Zigbee.connected())
+    {
+      Serial.print(".");
+      delay(100);
+    }
+    Serial.println();
+    Serial.println("Successfully connected to Zigbee network");
+
+    // Delay approx 1s (may be adjusted) to allow establishing proper connection with coordinator, needed for sleepy devices
+    delay(1000);
+
+    CheckButton();
+    SendSensorsValues(sensorsValues);
+    delay(1000);
   }
-  Serial.println();
-  Serial.println("Successfully connected to Zigbee network");
-
-  // Delay approx 1s (may be adjusted) to allow establishing proper connection with coordinator, needed for sleepy devices
-  delay(1000);
-
-  CheckButton();
-  readSensors();
-  delay(1000);
 
   Serial.println("Going to sleep now");
-  Serial.flush(); 
+  Serial.flush();
   digitalWrite(LED_BUILTIN, LOW);
   esp_deep_sleep_start();
 }
